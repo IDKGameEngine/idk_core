7b4ecb588b172b4a9528e04b0d576f285cc17993
mellic03 <mellic03@gmail.com>
2026-03-08 16:02:31 +1000

    Started platform to gfx conversion

---
+++ h/idk/core/engine.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "idk/core/type.hpp"
+
+
+namespace idk
+{
+    enum class EngineStat: uint8_t
+    {
+        Invalid = 0,
+        Alive,
+        Dead,
+        Starting,
+        Stopping,
+    };
+
+    enum class EngineCtrl: uint8_t
+    {
+        None = 0,
+        Start,
+        Stop
+    };
+
+    inline bool statIsCtrlEnabled(const idk::EngineStat stat)
+    {
+        // engine control only enabled during non-transition states.
+        switch (stat)
+        {
+            case EngineStat::Alive:
+            case EngineStat::Dead:
+                return true;
+            return false;
+        }
+    }
+
+    namespace core
+    {
+        class IEngine;
+    }
+}
+
+
+class idk::core::IEngine: public idk::NonCopyable, public idk::NonMovable
+{
+private:
+
+public:
+    virtual ~IEngine() = default;
+    virtual void shutdown() = 0;
+
+    /**
+     * @return true if successful/acknowledged.
+     */
+    virtual bool set_ctrl(EngineCtrl) = 0;
+    virtual EngineStat get_stat() = 0;
+
+};
+
